7083ed8ca262f18b895552d0187c6434696c2f81
Adarsh Ron <adarshron@gmail.com>
2021-01-16 16:27:21 +0530

    Added genesis

--- src/block.spec.ts
+++ src/block.spec.ts
@@ -1,5 +1,6 @@
 import IBlock from "./IBlock";
 import Block from "./block";
+import { GENESIS_BLOCK } from "../config";
 
 describe("Block", () => {
   const version: number = 1;
@@ -7,7 +8,7 @@ describe("Block", () => {
   const hashPrevBlock: string = "0000000000000000000000000000000000000000000000000000000000000000";
   const hashMerkleRoot: string = "3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A";
   const data: string = "The Times 23/Oct/2020 People Fear a Market Crash More Than They Have in Years";
-  const block: IBlock = {
+  const blockToAdd: IBlock = {
     version,
     time,
     hashPrevBlock,
@@ -15,13 +16,24 @@ describe("Block", () => {
     data,
   };
 
-  const result = new Block(block);
+  const block = new Block(blockToAdd);
 
   it("should have a version, time, previous hash, current hash and data", () => {
-    expect(result.version).toEqual(version);
-    expect(result.time).toEqual(time);
-    expect(result.hashPrevBlock).toEqual(hashPrevBlock);
-    expect(result.hashMerkleRoot).toEqual(hashMerkleRoot);
-    expect(result.data).toEqual(data);
-  })
+    expect(block.version).toEqual(version);
+    expect(block.time).toEqual(time);
+    expect(block.hashPrevBlock).toEqual(hashPrevBlock);
+    expect(block.hashMerkleRoot).toEqual(hashMerkleRoot);
+    expect(block.data).toEqual(data);
+  });
+
+  describe("genesisBlock()", () => {
+    const genesisBlock = Block.genesisBlock();
+    it("should return an instance of Block", () => {
+      expect(genesisBlock).toBeInstanceOf(Block);
+    });
+
+    it("should be same as the pre-defined genesis data", () => {
+      expect(genesisBlock).toMatchObject(GENESIS_BLOCK);
+    });
+  });
 });
